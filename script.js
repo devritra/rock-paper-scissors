9cff8324741b9e2f9ae079a26d6b03c4dab37f0b
Aritra Saha <158503009+devritra@users.noreply.github.com>
2025-07-03 14:50:35 +0530

    Create the function getHumanChoice
    created the function "getHumanChoice" that takes an input from the user and returns one of rock, paper, scissor based on the input

--- script.js
+++ script.js
@@ -12,3 +12,13 @@ function getComputerChoice(){
         return "scissor";
     }
 }
+function getHumanChoice(){
+    let choice = Number(prompt("Type 1 for rock\nType 2 for paper\nType 3 for scissor"));
+    if( choice === 1 ){
+        return "rock";
+    } else if( choice === 2 ){
+        return "paper";
+    } else if( choice === 3) {
+        return "scissor";
+    }
+}
